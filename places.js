37966beb0d7bbe4411c4822b19c845c6a8f524c9
箭野弘輔 <jianye.hongfu@gmail.com>
2023-08-11 10:57:23 +0900

    PlacesクラスのPromiseを消去

--- places.js
+++ places.js
@@ -7,76 +7,50 @@ export default class Places {
 
   async initialize() {
     this.places = await fetchPlace();
+    console.log(this.places);
     this.continents = this.places.filter(place => place.type === "PLACE_TYPE_CONTINENT");
     this.countries = this.places.filter(place => place.type === "PLACE_TYPE_COUNTRY");
     this.cities = this.places.filter(place => place.type === "PLACE_TYPE_CITY");
     this.airports = this.places.filter(place => place.type === "PLACE_TYPE_AIRPORT");
   }
 
-  async extractPlaceNames(place, parentEntityId){
-    return new Promise((resolve, reject) => {
-      const placesInParent = place.filter((place) => place.parentId === parentEntityId);
-      const placeNames = placesInParent.map(place => place.name);
-      if(!placesInParent) {
-        reject(new Error("Continent not found"));
-      } else {
-        resolve(placeNames);
-      }
-    });
+  async extractPlaceNames(place, parentEntityId, type){
+    const placesInParent = place.filter((place) => place.parentId === parentEntityId);
+    if(!placesInParent){ throw new Error(`${type} not found`); }
+    const placeNames = placesInParent.map(place => place.name);
+    return placeNames;
   }
 
   async extractContinentNames() {
-    return new Promise((resolve) => {
-      resolve(this.continents.map(continent => continent.name));
-    });
+    if(!this.continents){ throw new Error("Continent not found"); }
+    return this.continents.map(continent => continent.name)
   }
 
   async extractCountryNames(selectContinent) {
-    return new Promise((resolve, reject) => {
-      const choosedContinent = this.continents.find((continent) => continent.name === selectContinent)
-      this.countryNames = this.extractPlaceNames(this.countries, choosedContinent.entityId)
-      if(!choosedContinent){
-        reject(new Error("Continent not found"));
-      } else {
-        resolve(this.countryNames);
-      }
-    });
+    const choosedContinent = this.continents.find((continent) => continent.name === selectContinent)
+    if(!choosedContinent){ throw new Error("Continent not found"); }
+    this.countryNames = this.extractPlaceNames(this.countries, choosedContinent.entityId, "Country")
+    return this.countryNames;
   }
 
   async extractCityNames(selectCountry) {
-    return new Promise((resolve, reject) => {
-      const choosedCountry = this.countries.find((country) => country.name === selectCountry);
-      this.cityNames = this.extractPlaceNames(this.cities, choosedCountry.entityId);
-      if(!choosedCountry){
-        reject(new Error("Country not found"));
-      } else {
-        resolve(this.cityNames);
-      }
-    });
+    const choosedCountry = this.countries.find((country) => country.name === selectCountry);
+    if(!choosedCountry){ throw new Error("Country not found"); }
+    this.cityNames = this.extractPlaceNames(this.cities, choosedCountry.entityId, "City");
+    return this.cityNames;
   }
 
   async extractAirportNames(selectCity) {
-    return new Promise((resolve, reject) => {
-      const choosedCity = this.cities.find((city) => city.name === selectCity);
-      this.airportNames = this.extractPlaceNames(this.airports, choosedCity.entityId)
-      if(!choosedCity){
-        reject(new Error("City not found"));
-      } else {
-        resolve(this.airportNames);
-      }
-    });
+    const choosedCity = this.cities.find((city) => city.name === selectCity);
+    if(!choosedCity){ throw new Error("City not found"); }
+    this.airportNames = this.extractPlaceNames(this.airports, choosedCity.entityId, "Airport");
+    return this.airportNames;
   }
 
   async fetchAirportInfo(airportName){
-    return new Promise((resolve, reject) => {
-      const airport = this.airports.filter(
-        (airport) => airport.name === airportName
-      );
-      if(!airport) {
-        reject(new Error("Airport not found"))
-      } else {
-        resolve(airport)
-      }
-    });
+    const airport = this.airports.filter(
+      (airport) => airport.name === airportName
+    );
+    return airport
   }
 }
